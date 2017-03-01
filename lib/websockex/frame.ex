1f468c0ff487e98790b43acf4230b76db7cd75e5
Justin Baker <azolo1089@gmail.com>
2017-03-01 11:34:12 -0600

    Start Metaprogramming the Frame module
    - Add all less than 127 length frames

--- lib/websockex/frame.ex
+++ lib/websockex/frame.ex
@@ -3,12 +3,21 @@ defmodule WebSockex.Frame do
   Functions for parsing and encoding frames.
   """
 
-  @type opcode :: :ping
+  @data_codes %{text: 1,
+                binary: 2}
+
+  @control_codes %{close: 8,
+                   ping: 9,
+                   pong: 10}
+
+  @opcodes Map.merge(@data_codes, @control_codes)
+
+  @type opcode :: :text | :binary | :close | :ping | :pong
 
   defstruct [:opcode, :payload]
 
   @type t :: %__MODULE__{opcode: opcode,
-                         payload: binary}
+                         payload: binary | nil}
 
   @doc """
   Parses a bitstring and returns a frame.
@@ -17,11 +26,16 @@ defmodule WebSockex.Frame do
   def parse_frame(data) when bit_size(data) < 16 do
     {:incomplete, data}
   end
-  def parse_frame(<<1::1, 0::3, 9::4, 0::1, 0::7, buffer::bitstring>>) do
-    {%__MODULE__{opcode: :ping}, buffer}
+  for {key, opcode} <- @control_codes do
+    def parse_frame(<<1::1, 0::3, unquote(opcode)::4, 0::1, 0::7, buffer::bitstring>>) do
+      {%__MODULE__{opcode: unquote(key)}, buffer}
+    end
   end
-  def parse_frame(<<1::1, 0::3, 9::4, 0::1, len::7, rest::bitstring>>) do
-    <<payload::bytes-size(len), buffer::bitstring>> = rest
-    {%__MODULE__{opcode: :ping, payload: payload}, buffer}
+
+  for {key, opcode} <- @opcodes do
+    def parse_frame(<<1::1, 0::3, unquote(opcode)::4, 0::1, len::7, rest::bitstring>>) do
+      <<payload::bytes-size(len), buffer::bitstring>> = rest
+      {%__MODULE__{opcode: unquote(key), payload: payload}, buffer}
+    end
   end
 end
--- test/websockex/frame_test.exs
+++ test/websockex/frame_test.exs
@@ -9,8 +9,18 @@ defmodule WebSockex.FrameTest do
 
   alias WebSockex.{Frame}
 
+  @close_frame <<1::1, 0::3, 8::4, 0::1, 0::7>>
   @ping_frame <<1::1, 0::3, 9::4, 0::1, 0::7>>
+  @pong_frame <<1::1, 0::3, 10::4, 0::1, 0::7>>
+  @close_frame_with_payload <<1::1, 0::3, 8::4, 0::1, 5::7, "Hello">>
   @ping_frame_with_payload <<1::1, 0::3, 9::4, 0::1, 5::7, "Hello">>
+  @pong_frame_with_payload <<1::1, 0::3, 10::4, 0::1, 5::7, "Hello">>
+
+  @binary :erlang.term_to_binary :hello
+  @binary_size byte_size @binary
+
+  @text_frame <<1::1, 0::3, 1::4, 0::1, 5::7, "Hello"::utf8>>
+  @binary_frame <<1::1, 0::3, 2::4, 0::1, @binary_size::7, @binary::bytes>>
 
   describe "parse_frame" do
     test "returns incomplete when the frame is less than 16 bits" do
@@ -18,13 +28,36 @@ defmodule WebSockex.FrameTest do
       assert Frame.parse_frame(<<part>>) == {:incomplete, <<part>>}
     end
 
+    test "parses a close frame" do
+      assert Frame.parse_frame(@close_frame) == {%Frame{opcode: :close}, <<>>}
+    end
     test "parses a ping frame" do
       assert Frame.parse_frame(@ping_frame) == {%Frame{opcode: :ping}, <<>>}
     end
+    test "parses a pong frame" do
+      assert Frame.parse_frame(@pong_frame) == {%Frame{opcode: :pong}, <<>>}
+    end
 
+    test "parses a close frame with a payload" do
+      assert Frame.parse_frame(@close_frame_with_payload) ==
+        {%Frame{opcode: :close, payload: "Hello"}, <<>>}
+    end
     test "parses a ping frame with a payload" do
       assert Frame.parse_frame(@ping_frame_with_payload) ==
         {%Frame{opcode: :ping, payload: "Hello"}, <<>>}
+    end
+    test "parses a pong frame with a payload" do
+      assert Frame.parse_frame(@pong_frame_with_payload) ==
+        {%Frame{opcode: :pong, payload: "Hello"}, <<>>}
+    end
+
+    test "parses a text frame" do
+      assert Frame.parse_frame(@text_frame) ==
+        {%Frame{opcode: :text, payload: "Hello"}, <<>>}
+    end
+    test "parses a binary frame" do
+      assert Frame.parse_frame(@binary_frame) ==
+        {%Frame{opcode: :binary, payload: @binary}, <<>>}
     end
 
     test "returns overflow buffer" do
