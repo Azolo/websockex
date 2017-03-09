c28a8fb752a3621ee0c5cdd665a6a78882e7125b
Justin Baker <azolo1089@gmail.com>
2017-03-09 11:44:26 -0600

    Encode ping frame with a payload
    - Masking! It's a thing

--- lib/websockex/frame.ex
+++ lib/websockex/frame.ex
@@ -3,6 +3,8 @@ defmodule WebSockex.Frame do
   Functions for parsing and encoding frames.
   """
 
+  import Bitwise
+
   @type opcode :: :text | :binary | :close | :ping | :pong
   @type close_code :: 1000..4999
 
@@ -27,10 +29,29 @@ defmodule WebSockex.Frame do
   def encode_frame({:ping, <<payload::binary>>}) when byte_size(payload) > 125 do
     {:error, %WebSockex.FrameEncodeError{reason: :control_frame_too_large,
                                          frame_type: :ping,
-                                         frame_payload: payload}} 
+                                         frame_payload: payload}}
+  end
+  def encode_frame({:ping, <<payload::binary>>}) do
+    mask = create_mask()
+    len = byte_size(payload)
+    masked_payload = mask(mask, payload)
+    {:ok, <<1::1, 0::3, 9::4, 1::1, len::7, mask::bytes-size(4), masked_payload::binary-size(len)>>}
   end
 
-  def create_mask do
+  defp create_mask do
     :crypto.strong_rand_bytes(4)
+  end
+
+  defp mask(key, payload, acc \\ <<>>)
+  defp mask(_, <<>>, acc), do: acc
+  for x <- 1..3 do
+    defp mask(<<key::8*unquote(x), _::binary>>, <<part::8*unquote(x)>>, acc) do
+      masked = part ^^^ key
+      <<acc::binary, masked::8*unquote(x)>>
+    end
+  end
+  defp mask(<<key::8*4>>, <<part::8*4, rest::binary>>, acc) do
+    masked = part ^^^ key
+    mask(<<key>>, rest, <<acc::binary, masked::8*4>>)
   end
 end
--- test/websockex/frame_test.exs
+++ test/websockex/frame_test.exs
@@ -10,6 +10,20 @@ defmodule WebSockex.FrameTest do
   # << fin::1, 0::3, opcode::4, 1::1, payload_len::(7-71), masking_key::32 >>
 
   alias WebSockex.{Frame}
+  import Bitwise
+
+  def unmask(key, payload, acc \\ <<>>)
+  def unmask(_, <<>>, acc), do: acc
+  for x <- 1..3 do
+    def unmask(<<key::8*unquote(x), _::binary>>, <<payload::8*unquote(x)>>, acc) do
+      part = payload ^^^ key
+      <<acc::binary, part::8*unquote(x)>>
+    end
+  end
+  def unmask(<<key::8*4>>, <<payload::8*4, rest::binary>>, acc) do
+    part = payload ^^^ key
+    unmask(<<key>>, rest, <<acc::binary, part::8*4>>)
+  end
 
   @large_binary <<0::300*8, "Hello">>
 
@@ -29,6 +43,13 @@ defmodule WebSockex.FrameTest do
     test "encodes a ping frame" do
       assert {:ok, <<1::1, 0::3, 9::4, 1::1, 0::7, _::32>>} =
         Frame.encode_frame(:ping)
+    end
+    test "encodes a ping frame with a payload" do
+      payload = "Hello"
+      len = byte_size(payload)
+      assert {:ok, <<1::1, 0::3, 9::4, 1::1, ^len::7, mask::bytes-size(4), masked_payload::binary-size(len)>>} =
+        Frame.encode_frame({:ping, payload})
+      assert unmask(mask, masked_payload) == payload
     end
 
     test "raises an error with large ping frame" do
