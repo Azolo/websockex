4a3d8ea485442d12fe66b157c16f245e5f95b591
Justin Baker <azolo1089@gmail.com>
2017-03-19 11:13:10 -0500

    Add longer even timeout to handle_disconnect
    - I don't know why it's not working consistently

--- test/websockex/client_test.exs
+++ test/websockex/client_test.exs
@@ -252,25 +252,25 @@ defmodule WebSockex.ClientTest do
     test "is invoked when receiving a close frame", context do
       send(context.server_pid, :close)
 
-      assert_receive :caught_disconnect, 250
+      assert_receive :caught_disconnect, 750
     end
 
     test "is invoked when receiving a close frame with a payload", context do
       send(context.server_pid, {:close, 4025, "Testing"})
 
-      assert_receive {:caught_disconnect, 4025, "Testing"}, 250
+      assert_receive {:caught_disconnect, 4025, "Testing"}, 750
     end
 
     test "is invoked when sending a close frame", context do
       WebSockex.Client.cast(context.pid, :close)
 
-      assert_receive :caught_disconnect, 250
+      assert_receive :caught_disconnect, 750
     end
 
     test "is invoked when sending a close frame with a payload", context do
       WebSockex.Client.cast(context.pid, {:close, 4025, "Testing"})
 
-      assert_receive {:caught_disconnect, 4025, "Testing"}, 250
+      assert_receive {:caught_disconnect, 4025, "Testing"}, 750
     end
   end
 
