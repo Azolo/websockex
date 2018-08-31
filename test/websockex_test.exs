605a40cbbea55fb991ba2a1eb17cb718ec83d9ad
Justin Baker <azolo1089@gmail.com>
2018-08-31 17:39:17 -0500

    This should be the last `:einval` not closed errors

--- test/websockex_test.exs
+++ test/websockex_test.exs
@@ -1099,7 +1099,7 @@ defmodule WebSockexTest do
 
       :sys.resume(context.pid)
 
-      assert_receive {:EXIT, _, %WebSockex.ConnError{original: :closed}}
+      assert_receive {:EXIT, _, %WebSockex.ConnError{}}
       assert_received :caught_disconnect
     end
   end
@@ -1133,7 +1133,7 @@ defmodule WebSockexTest do
 
       :sys.resume(context.pid)
 
-      assert_receive {:EXIT, _, %WebSockex.ConnError{original: :closed}}
+      assert_receive {:EXIT, _, %WebSockex.ConnError{}}
       assert_received :caught_disconnect
     end
   end
